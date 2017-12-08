948bf5cdf7e9f8bf2532e663de4923ef2dfc1e3e
Eugene Epishev <eepishev1989@gmail.com>
2017-12-08 21:59:47 +0200

    Вычисление растояния между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,23 +5,31 @@ public class MyFirstProgram {
   public static void main(String[] args) {
     hello("world");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+         /* Реализация через функцию
+   Point p1 = new Point(7,3);
+   Point p2 = new Point(6,7);
+   System.out.println("Растояние между двумя координатами = "+ distance(p1,p2));
+    */
+
+    Point p = new Point(8, 8);
+    System.out.println("Растояние между двумя координатами = " + p.distance(4, 4));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello" + somebody);
   }
 
-  public static double area(double len) {
-    return len * len;
+       /* Функция
+  public static double distance(Point p1, Point p2) {
+   return = Math.sqrt(Math.pow((p1.x-p2.x),2)+Math.pow((p1.y-p2.y),2));
   }
+   */
 
-  public static double area(double a, double b) {
-    return a * b;
-  }
+
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  double x;
+  double y;
+
+  public Point(double x,double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance(double x, double y) {
+    return Math.sqrt(Math.pow((this.x - x),2) + Math.pow((this.y - y),2));
+  }
+}
