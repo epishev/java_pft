a38671c70f73aee65f0035f88c3bd58b1b9ac049
Eugene Epishev <eepishev1989@gmail.com>
2017-12-11 21:51:03 +0200

    Запуск тестов в разных браузерах(сравнение через equals() )

--- addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.safari.SafariDriver;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -25,11 +26,11 @@ public class ApplicationManager {
 
 
   public void init() {
-    if (browser == BrowserType.FIREFOX) {
+    if (Objects.equals(browser, BrowserType.FIREFOX)) {
       wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-    } else if (browser == BrowserType.CHROME) {
+    } else if (Objects.equals(browser, BrowserType.CHROME)) {
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.SAFARI) {
+    } else if (Objects.equals(browser, BrowserType.SAFARI)) {
       wd = new SafariDriver();
     }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
