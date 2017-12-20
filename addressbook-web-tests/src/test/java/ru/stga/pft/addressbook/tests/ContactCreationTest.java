94a5783e1b4b1f775db6bcea2263645f1b4b251e
Eugene Epishev <eepishev1989@gmail.com>
2017-12-20 02:25:40 +0200

    Реализованы лямбда выражения для поиска максимального элемента

--- addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/tests/ContactCreationTest.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stga.pft.addressbook.model.ContactData;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,18 +13,12 @@ public class ContactCreationTest extends TestBase {
   public void testContactCreation() {
     app.getNavigationHelper().goToHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("test1", "test2", "test3", "123456", "test@test.com", "test1");
+    ContactData contact = new ContactData("test4", "test2", "test3", "123456", "test@test.com", "test1");
     app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
