c61bf6a42b6d20adc5d13e72535c233e2e9de9d4
Eugene Epishev <eepishev1989@gmail.com>
2018-01-22 23:02:29 +0200

    Ізвлечение информации из БД с помощью Hibernate

--- addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/model/GroupData.java
@@ -3,16 +3,31 @@ package ru.stga.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @XStreamAlias("group")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
   @XStreamOmitField
+  @Id
+  @Column(name = "group_id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name = "group_name")
   private String name;
   @Expose
+  @Column(name = "group_header")
+  @Type(type = "text")
   private String header;
   @Expose
+  @Column(name = "group_footer")
+  @Type(type = "text")
   private String footer;
 
 
@@ -54,14 +69,6 @@ public class GroupData {
 
 
   @Override
-  public String toString() {
-    return "GroupData{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            '}';
-  }
-
-  @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
@@ -77,6 +84,16 @@ public class GroupData {
     int result = id;
     result = 31 * result + (name != null ? name.hashCode() : 0);
     return result;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", header='" + header + '\'' +
+            ", footer='" + footer + '\'' +
+            '}';
   }
 
 }
