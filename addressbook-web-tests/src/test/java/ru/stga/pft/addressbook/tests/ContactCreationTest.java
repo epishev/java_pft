38c3cb011627b4c5c1fa929982704e2682ca3e44
Eugene Epishev <eepishev1989@gmail.com>
2018-02-01 18:10:11 +0200

    Реализованны отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stga/pft/addressbook/tests/ContactCreationTest.java
@@ -63,6 +63,7 @@ public class ContactCreationTest extends TestBase {
     Contacts after = app.db().contacts();
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    verifyContactListInUI();
   }
 
 }
